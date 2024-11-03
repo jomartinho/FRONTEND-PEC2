b5a8e23c0e7279191f0ab8ac74f55edc8fbc29a1
Joao Martinho <joao.rcsmartinho@gmail.com>
2024-11-03 17:18:07 +0000

    changing npm, adding mocha, finishing ejercicio 3.a

--- PEC2_Ej3/a_map/core.js
+++ PEC2_Ej3/a_map/core.js
@@ -1,22 +1,22 @@
 function multiplyBy10(array) {
-  // your code here
+  return array.map((num) => num * 10);
 }
 
 function shiftRight(array) {
-  // your code here
+  return array.map((_, i, arr) => arr[(i - 1 + arr.length) % arr.length]);
 }
 
 function onlyVowels(array) {
-  // your code here
+  return array.map((str) => str.replace(/[^aeiouAEIOU]/g, ""));
 }
 
 function doubleMatrix(array) {
-  // your code here
+  return array.map((subArray) => subArray.map((num) => num * 2));
 }
 
 module.exports = {
   multiplyBy10,
   shiftRight,
   onlyVowels,
-  doubleMatrix
+  doubleMatrix,
 };
